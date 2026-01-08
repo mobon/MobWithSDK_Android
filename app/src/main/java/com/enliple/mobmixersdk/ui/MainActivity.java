c99be710016e3d9be69becd9c332bfedb5c07fba
hj.lee <hj.lee@mobwith.co.kr>
2026-01-08 16:03:24 +0900

    update: 1.0.70 - 스마트 프레임 Banner Ad 기능 추가

--- app/src/main/java/com/enliple/mobmixersdk/ui/MainActivity.java
+++ app/src/main/java/com/enliple/mobmixersdk/ui/MainActivity.java
@@ -87,6 +87,13 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
             }
         });
 
+        binding.btnSmartFrameAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SmartFrameAdActivity.class));
+            }
+        });
+
     }
 
     @Override
---
+++ app/src/main/java/com/enliple/mobmixersdk/ui/SmartFrameAdActivity.java
@@ -0,0 +1,119 @@
+package com.enliple.mobmixersdk.ui;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.mobwith.manager.nativeadview.NativeAdViewItemModel;
+import com.mobwith.sdk.MobwithFrameBannerView;
+import com.mobwith.sdk.R;
+import com.mobwith.sdk.callback.iBannerCallback;
+import com.mobwith.sdk.databinding.ActivityFrameAdBinding;
+import com.mobwith.sdk.utils.MainThreadHandler;
+
+public class SmartFrameAdActivity extends BaseActivity<ActivityFrameAdBinding> {
+    private String mUnitId = "10892178";
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_frame_ad;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MobwithFrameBannerView bannerView = new MobwithFrameBannerView(this,
+                mUnitId,
+                binding.adviewContainer,
+                new NativeAdViewItemModel(
+                        R.layout.custom_native_ad_view,
+                        R.id.mediaContainerView,
+                        R.id.imageViewAD,
+                        R.id.imageViewLogo,
+                        R.id.textViewTitle,
+                        R.id.textViewDesc,
+                        R.id.buttonGo,
+                        R.id.infoViewLayout,
+                        R.id.imageViewInfo
+                ),
+                new NativeAdViewItemModel(
+                        R.layout.custom_native_ad_view_2,
+                        R.id.mediaContainerView,
+                        R.id.imageViewAD,
+                        R.id.imageViewLogo,
+                        R.id.textViewTitle,
+                        R.id.textViewDesc,
+                        R.id.buttonGo,
+                        R.id.infoViewLayout,
+                        R.id.imageViewInfo
+                ));
+
+        bannerView.setAdListener(new iBannerCallback() {
+            @Override
+            public void onLoadedAdInfo(boolean result, String errorStr) {
+                MainThreadHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (result){
+                            Toast.makeText(SmartFrameAdActivity.this, "광고 로드 성공", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(SmartFrameAdActivity.this, "광고 로드 실패 - " + errorStr, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onAdClicked() {
+                Toast.makeText(SmartFrameAdActivity.this, "광고 클릭", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getOnBackPressedDispatcher().onBackPressed();
+            }
+        });
+
+        binding.editTextBannerUnitID.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mUnitId = s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        binding.buttonLoadAD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bannerView.setUnitID(mUnitId);
+                bannerView.loadAd();
+            }
+        });
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
